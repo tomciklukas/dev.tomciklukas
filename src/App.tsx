2a7925735f1ab114506316fba7d836d2d80c25ec
Lukas Tomcik <lukas.tomcik@bimproject.cz>
2024-12-11 22:08:23 +0100

    refactor: replace webpack with parcel, remove eslint and normalize

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
     return (
         <div>
-            <style>{ `span { margin: 0 1rem; }` }</style>
+            <style>{ "span { margin: 0 1rem; }" }</style>
             <div>
                 <span>Lukas Tomcik</span>
             </div>
